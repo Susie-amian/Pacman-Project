99651d14b672a72b652ecb5cb00aae72194567e4
Susie Zhang <s.zhang121@student.unimelb.edu.au>
2020-10-11 02:52:48 +1100

    adding feature draft

--- myTeamFeature.py
+++ myTeamFeature.py
@@ -85,27 +85,36 @@ class QlearningAgent(CaptureAgent):
 
     self.start = gameState.getAgentPosition(self.index)
     self.totalFoodList = self.getFood(gameState).asList()
+    self.eatenFood = []
 
     # recording dict
-    self.weights = util.Counter({
+    self.weights = util.Counter()
+    
+
+    #self.weights['successorScore'] = 1
+    #self.weights['distanceToFood'] = -1
+    self.weights['distToFood'] = 1
+    #self.weights['eatenFood'] = 10
+    """self.weights = util.Counter({
       'successorScore': 100,
       'distToFood': 10,
       'ghostDistance': 10,
       'stop': -1,
       'powerPelletValue': 10,
-      'backToSafeZone': -1,
+      'backToSafeZone': 1,
       'numInvaders': -10,
       'onDefense': 0.5,
       'invaderDistance': -10,
       'stop': -10, 
-      'reverse': -2
-    })
+      'reverse': -2,
+      'eatenFood':10
+    })"""
 
-    self.goalFood, self.DistGoalFood = self.getDistToFood(gameState)
+    #self.goalFood, self.DistGoalFood = self.getDistToFood(gameState)
     
     # 
     self.threatenedDistance = 5
-    self.minPelletsToCashIn = 8
+    self.minPelletsToCashIn = 5
     self.distanceToTrackPowerPelletValue = 3
     # dictionary of (position) -> [action, ...]
     # populated as we go along; to use this, call self.getLegalActions(gameState)
@@ -114,6 +123,51 @@ class QlearningAgent(CaptureAgent):
     self.lastNumReturnedPellets = 0.0
 
     self.defenseTimer = 0.0
+    self.initFoodList = self.getFood(gameState).asList()
+    self.eatenFood = set()
+
+
+    # get layout frontier
+    self.width = gameState.data.layout.width
+    self.height = gameState.data.layout.height
+    #print([(i, j) for i in range(self.width) for j in range(self.height)])
+
+    self.midPointLeft = int((self.width / 2.0)-1)
+    self.midPointRight = int((self.width / 2.0)+1)
+    self.isRed = gameState.isOnRedTeam(self.index)
+
+    if self.isRed:
+      self.midPoint = int(self.midPointLeft)
+      self.enemyCells = []
+      for i in range(self.midPointRight, self.width):
+        for j in range(self.height):
+          if not gameState.hasWall(i, j):
+            
+            self.enemyCells.append((i, j))
+      
+    else:
+      self.midPoint = int(self.midPointRight)
+      self.enemyCells = []
+      for i in range(self.midPointLeft):
+        for j in range(self.height):
+
+          if not gameState.hasWall(i, j):
+            
+            self.enemyCells.append((int(i), int(j)))
+
+    self.frontierPoints = [(self.midPoint, int(i)) for i in range(self.height) if not gameState.hasWall(self.midPoint, i)]
+    self.distToHome = self.getDistToHome(self.frontierPoints, self.enemyCells)
+    # get shortest path to frontier point
+    #self.frontierState, self.actionFrontier = self.toFrontier(gameState, self.frontierPoints, self.start)
+
+  def getDistToHome(self, home, possibleLocs):
+      distToHome = {}
+      for h in home:
+        for loc in possibleLocs:
+          distToHome[loc] = self.getMazeDistance(h, loc)
+      return distToHome
+
+    
 
   def chooseAction(self, gameState):
     """
@@ -131,6 +185,17 @@ class QlearningAgent(CaptureAgent):
 
     maxValue = max(values)
     bestActions = self.getBestActions(actions, maxValue, values)
+
+    # number of food eaten before go home
+    myState = gameState.getAgentState(self.index)
+    myPos = myState.getPosition()
+    if myPos in self.initFoodList:
+      self.eatenFood.add(myPos)
+     
+    # if go home, eaten food num start from 0 
+    if not myState.isPacman:
+      #features['eatenFoodNum'] = 0
+      self.eatenFood = set()
 
     
     # Case 1: food left <= 2
@@ -151,6 +216,7 @@ class QlearningAgent(CaptureAgent):
 
     # update the weight
     self.update(gameState, actionToReturn, successor)
+
 
     # return the action
     return actionToReturn
@@ -200,18 +266,37 @@ class QlearningAgent(CaptureAgent):
 
     return Qval
 
+ 
   def getFeatures(self, gameState, action):
     features = util.Counter()
+    
+    curPos = gameState.getAgentPosition(self.index)
+    curFoodList = self.getFood(gameState).asList()
+
     successor = self.getSuccessor(gameState, action)
-    myState = successor.getAgentState(self.index)
-    foodList = self.getFood(successor).asList()
-    myPos = myState.getPosition()
+    sucState = successor.getAgentState(self.index)
+    sucFoodList = self.getFood(successor).asList()
+    sucPos = successor.getAgentPosition(self.index)
     # Score
-    features['successorScore'] = self.getScore(successor)  # self.getScore(successor)
+    #features['successorScore'] = self.getScore(successor)  # self.getScore(successor)
+    features['successorScore'] = len(self.initFoodList)-len(sucFoodList)
       
     # Distance to closest food
-    _, minDist = self.getDistToFood(successor)
+
+    _, minDist = self.getDistToFood(successor,sucFoodList)
+    #features['eatenFood'] = len(self.eatenFood)
+    #features['distToFood'] = minDist
     features['distToFood'] = 1/minDist
+    #diffFoodList = list(set(curFoodList) - set(sucFoodList))
+    if sucPos in self.eatenFood:
+      features['distToFood'] = 2
+      #features['eatenFood'] = 1
+
+
+
+    # need to cash in
+    features['cashIn'] = self.getCashInValue(sucPos, sucState)
+ 
 
     # Get all enemies
     enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
@@ -220,14 +305,17 @@ class QlearningAgent(CaptureAgent):
     scaredGhosts = [a for a in enemies if not a.isPacman and a.getPosition() != None and a.scaredTimer > 0]
 
     # Computes whether we're on defense (1) or offense (0)
-    features['onDefense'] = 1
-    if myState.isPacman: features['onDefense'] = 0
-
+    """features['onDefense'] = 1
+    if myState.isPacman: 
+      features['onDefense'] = 0
+    """
     # Computes distance to invaders we can see
-    features['numInvaders'] = len(invaders)
-    if len(invaders) > 0:
-      dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
-      features['invaderDistance'] = min(dists)
+    #features['numInvaders'] = len(invaders)
+    numInvaders = len(invaders)
+    
+    """if len(invaders) > 0:
+      dists = [self.getMazeDistance(sucPos, a.getPosition()) for a in invaders]
+      features['invaderDistance'] = min(dists)"""
 
     # Heavily prioritize not stopping
     if action == Directions.STOP: 
@@ -236,18 +324,39 @@ class QlearningAgent(CaptureAgent):
     
 
     # distance to powerPellet
-    features['powerPelletValue'] = self.getPowerPelletValue(myPos, successor, scaredGhosts)
+    features['powerPelletValue'] = self.getPowerPelletValue(sucPos, successor, scaredGhosts)
 
     # Adding value for cashing in pellets
-    features['backToSafeZone'] = self.getCashInValue(myPos, gameState, myState)
+    #features['backToSafeZone'] = self.getCashInValue(sucPos, gameState, sucState)
     
     # Adding value for going back home
-    features['backToSafeZone'] += self.getBackToStartDistance(myPos, features['ghostDistance'])
+    features['backToSafeZone'] += self.getBackToStartDistance(sucPos, features['ghostDistance'])
+
+     
+
+
+    
+
+
+    
+
+
+
+    #distance to the ghost
+    #features['ghostDistance']
 
     if self.shouldRunHome(gameState):
-      features['backToSafeZone'] = self.getMazeDistance(self.start, myPos) * 10000
+      features['backToSafeZone'] = self.getMazeDistance(self.start, sucPos) 
 
     return features
+
+  def getCashInValue(self, myPos, nextState):
+    # if we have enough pellets, attempt to cash in
+    if nextState.numCarrying >= self.minPelletsToCashIn:
+      return self.distToHome[myPos]
+    else:
+      return 0
+
     
   # If there are not any scared ghosts, then we value eating pellets
   def getPowerPelletValue(self, myPos, successor, scaredGhosts):
@@ -274,7 +383,7 @@ class QlearningAgent(CaptureAgent):
     if smallestGhostPosition > self.threatenedDistance or smallestGhostPosition == 0:
       return 0
     else:
-      return self.getMazeDistance(self.start, myPos) * 1000
+      return self.getMazeDistance(self.start, myPos) 
 
   def getLegalActions(self, gameState):
     """
@@ -292,17 +401,11 @@ class QlearningAgent(CaptureAgent):
     else:
       return -1 * gameState.getScore()
 
-  def getCashInValue(self, myPos, gameState, myState):
-      # if we have enough pellets, attempt to cash in
-      if myState.numCarrying >= self.minPelletsToCashIn:
-        return self.getMazeDistance(self.start, myPos)
-      else:
-        return 0
-  # #
+  
 
-  def getDistToFood(self, currentState):
+  def getDistToFood(self, currentState, foodList):
     pos = currentState.getAgentState(self.index).getPosition()
-    foodList =  self.getFood(currentState).asList()
+    #foodList =  self.getFood(currentState).asList()
     min_dist = 9999
 
     for food in foodList:
@@ -345,24 +448,31 @@ class QlearningAgent(CaptureAgent):
 
     # better score
     if sucState.getScore() > curState.getScore():
-      reward += 20
+      reward += 100
     
     # food difference
-    foodDifference = sucFoodNum - curFoodNum
+    foodDifference = curFoodNum - sucFoodNum
     if (foodDifference > 0):
-      reward += 1000
+      reward += 30
     elif (foodDifference < 0):
       reward += foodDifference*2
 
     # closer distance to capsule
 
     # closer distance to food
-    if self.goalFood == sucPos:
+    foodList = self.getFood(gameState).asList()
+    curToFood = abs(min([self.getMazeDistance(curPos, food) for food in foodList]))
+    sucToFood = abs(min([self.getMazeDistance(sucPos, food) for food in foodList]))
+    if curToFood > sucToFood:
+        reward += 10
+    elif curToFood < sucToFood:
+        reward -= 10
+    """if self.goalFood == sucPos:
       self.goalFood, sucDist = self.getDistToFood(sucState)
     else:
       sucDist = self.getMazeDistance(sucPos, self.goalFood)
    
-    reward += self.getFoodProximityReward(curPos, sucDist, self.goalFood)
+    reward += self.getFoodProximityReward(curPos, sucDist, self.goalFood)"""
     return reward
 
 
@@ -381,5 +491,13 @@ class QlearningAgent(CaptureAgent):
     learnedWeight = self.alpha*(reward + self.discount * newMaxQval - oldQ)  #value 
     features = self.getFeatures(gameState, action)  #counter
 
+    pos = gameState.getAgentState(self.index).getPosition()
+   
+    
+
     for key in features.keys():
       self.weights[key] += learnedWeight * features[key]
+    print("===FEATURE===388",pos,self.index)
+    print(self.weights)
+    print(features)
+    print(reward)
